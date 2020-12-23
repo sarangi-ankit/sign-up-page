010d28d177df0ce46b4720f9d3eb721d6da32f72
Ankit Sarangi <73221869+sarangi-ankit@users.noreply.github.com>
2020-12-23 19:14:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+
+const app=express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+});
+app.post("/",function(req,res){
+  const firstName=req.body.fname;
+  const lastName=req.body.lname;
+  const email=req.body.email;
+  const data={
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME:firstName,
+          LNAME:lastName
+        }
+      }
+    ]
+  };
+  const jsonData=JSON.stringify(data);
+  const url="https://us7.api.mailchimp.com/3.0/lists/8ad3242431";
+
+  const options={
+    method:"POST",
+    auth:"ankit:s06085e43ddc9619b080422bcdf38a497-us7"
+  }
+  const request=https.request(url,options,function(response){
+    if (response.statusCode===200){
+      res.sendFile(__dirname + "/success.html")
+    }
+    else{
+      res.sendFile(__dirname + "/failure.html")
+    }
+
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+
+app.listen(3000,function(){
+  console.log("server is running on port 3000");
+});
+
+//api key
+//06085e43ddc9619b080422bcdf38a497-us7
+
+//list id
+//8ad3242431
